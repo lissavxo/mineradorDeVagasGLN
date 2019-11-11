648f79ca3c8bd52ceb9cd574e0b3cd6a0a8b954d
Lissa Ximenes <lissavitoria12@gmail.com>
2019-11-11 21:43:03 +0000

    Documentação e alteração do nome da função emaranhado e variáveis de controle.

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+#Lista cotendo empresa, local e área
+lista_empresa_local_area = []
 
 def get_soup():
     url = 'https://empregos.profissionaisti.com.br/vagas/distrito-federal/'
@@ -31,7 +33,7 @@ def data_vagas(soup):
 
 
 #Lista contendo empresa, local e area da vaga
-def emaranhado(soup): #
+def empresa_local_area(soup): #
     lista = []
     for item in soup.find_all('div', class_="job-icons clearfix"):
         for atributo in item.find_all('a'):
@@ -41,30 +43,32 @@ def emaranhado(soup): #
     return lista
 
 
-x = []
-x = emaranhado(soup)
-y = len(x)
+#Varíavel contendo a lista que retorna da varíavel empresa_local_area()
+lista_empresa_local_area = empresa_local_area(soup)
+
+#Variável dinâmica contendo o tamanho da lista da variável empresa_local_area
+controle_tamanho = len(lista_empresa_local_area)
 
 
 #Retorna empresa da vaga
 def empresa():
     empresa_vaga = []
-    for k in range(0, y, 3):
-        empresa_vaga.append(x[k])
+    for indice in range(0, controle_tamanho, 3):
+        empresa_vaga.append(lista_empresa_local_area[indice])
     return empresa_vaga
 
 
 #Retorna local da vaga
 def local():
     local_vaga = []
-    for item in range(1, y, 3):
-        local_vaga.append(x[item])
+    for indice in range(1, controle_tamanho, 3):
+        local_vaga.append(lista_empresa_local_area[indice])
     return local_vaga
 
 
 #Retorna área da vaga
 def area():
     area_vaga = []
-    for item in range(2, y, 3):
-        area_vaga.append(x[item])
+    for indice in range(2, controle_tamanho, 3):
+        area_vaga.append(lista_empresa_local_area[indice])
     return area_vaga
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import scraping
+import funcoes
 
 
 scraping.getVagas()
