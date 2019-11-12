1154c219a5897b6ef79521546e5d96b045effb01
Lissa Ximenes <lissavitoria12@gmail.com>
2019-11-12 17:41:17 +0000

    Update all

--- funcoes.py
+++ funcoes.py
@@ -1,45 +1,50 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_soup(url):
-  
-  site = requests.get(url)
-  soup = BeautifulSoup(site.content, 'html.parser')
-  return soup
+
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    return soup
+
 
 #funçao para retornar lista de soups box da vaga
 def box_soup(soup):
-  box_list = []
-  for box in soup.find('ul', class_="job-list"):
-    box_list.append(box)
-  return box_list
+    box_list = []
+    for box in soup.find('ul', class_="job-list"):
+        box_list.append(box)
+    return box_list
+
+
 #funcao retorna lista de titulos
 def get_titles(box_soup):
-  titles = []
-  for box in box_soup:
-    text = box.h4.get_text()
-    titles.append(text) 
-  return titles 
+    titles = []
+    for box in box_soup:
+        text = box.h4.get_text()
+        titles.append(text)
+    return titles
+
 
 #funcao retorna lista de links de vaga
 def get_links(soup):
-  links = []
-  links_soup = BeautifulSoup(str( soup.find_all('h4')), 'html.parser' )
-  for link in links_soup.select('a'):
-    links.append(link.get('href'))
-  return links
+    links = []
+    links_soup = BeautifulSoup(str(soup.find_all('h4')), 'html.parser')
+    for link in links_soup.select('a'):
+        links.append(link.get('href'))
+    return links
+
 
 #funcao retorna lista de codigos da vaga
 def get_codes(box_soup):
-  codes = []
-  for box in box_soup:
-    code = list(box.get('id'))
-    del code[0:5]
-    code = ''.join(code)
-    codes.append(int(code))
+    codes = []
+    for box in box_soup:
+        code = list(box.get('id'))
+        del code[0:5]
+        code = ''.join(code)
+        codes.append(int(code))
 
-  return codes 
-
+    return codes
 
 
 #Retorna o regime de trabalho e o período
@@ -60,9 +65,8 @@ def data_vagas(soup):
     return data_vagas
 
 
-
-#Lista contendo empresa, local e area da vaga
-def empresa_local_area(soup): #
+#Lista contendo empresa, local e tag da vaga
+def empresa_local_area(soup):  #
     lista = []
     for item in soup.find_all('div', class_="job-icons clearfix"):
         for atributo in item.find_all('a'):
@@ -78,7 +82,7 @@ def empresa(empresa_local_area):
     controle_tamanho = len(empresa_local_area)
     for indice in range(0, controle_tamanho, 3):
         item = list(empresa_local_area[indice])
-        del item [0:9]
+        del item[0:9]
         item = ''.join(item)
         empresa_vaga.append(item)
     return empresa_vaga
@@ -90,19 +94,19 @@ def local(empresa_local_area):
     controle_tamanho = len(empresa_local_area)
     for indice in range(1, controle_tamanho, 3):
         item = list(empresa_local_area[indice])
-        del item [0:9]
+        del item[0:9]
         item = ''.join(item)
         local_vaga.append(item)
     return local_vaga
 
 
-#Retorna área da vaga
-def area(empresa_local_area):
-    area_vaga = []
+#Retorna tag da vaga
+def tag(empresa_local_area):
+    tag_vaga = []
     controle_tamanho = len(empresa_local_area)
     for indice in range(2, controle_tamanho, 3):
         item = list(empresa_local_area[indice])
-        del item [0:11]
+        del item[0:11]
         item = ''.join(item)
-        area_vaga.append(item)
-    return area_vaga
+        tag_vaga.append(item)
+    return tag_vaga
--- main.py
+++ main.py
@@ -2,25 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 import funcoes
 
-
-soup = funcoes.get_soup('https://empregos.profissionaisti.com.br/vagas/distrito-federal/') 
+soup = funcoes.get_soup(
+    'https://empregos.profissionaisti.com.br/vagas/distrito-federal/')
 
 titles = funcoes.get_titles(funcoes.box_soup(soup))
 links = funcoes.get_links(soup)
 codes = funcoes.get_codes(funcoes.box_soup(soup))
 company = funcoes.empresa(funcoes.empresa_local_area(soup))
 place = funcoes.local(funcoes.empresa_local_area(soup))
-area = funcoes.area(funcoes.empresa_local_area(soup))
+tag = funcoes.tag(funcoes.empresa_local_area(soup))
 
-for i in range(len(titles)-1):
-  
-  print(" ")
-  print("---------------------------------------------------")
-  print("Titulo: {}".format(titles[i]))
-  print("link: {}".format(links[i]))
-  print("code: {}".format(codes[i]))
-  print("company: {}".format(company[i]))
-  print("place: {}".format(place[i]))
-  print("area: {}".format(area[i]))
-  print("---------------------------------------------------")
-  print (" ")
+for i in range(len(titles) - 1):
+
+    print(" ")
+    print("---------------------------------------------------")
+    print("Titulo: {}".format(titles[i]))
+    print("link: {}".format(links[i]))
+    print("code: {}".format(codes[i]))
+    print("company: {}".format(company[i]))
+    print("place: {}".format(place[i]))
+    print("tag: {}".format(tag[i]))
+    print("---------------------------------------------------")
+    print(" ")
