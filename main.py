7969612f88d30cc556b4af2a16f6fa8ffc3a23d6
Guilherme Ferreira <guilherme.schiffer@gmail.com>
2019-11-27 11:33:47 -0300

    Incremento de um menu de opções

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import scraping
 import bot
-vagas = scraping.get_vagas()
 
-scraping.vagas_to_dict(vagas)
+print("\nOpções:\n1 - Fazer o scraping das vagas\n2 - Enviar para o canal\n")
+entrada = int(input("Selecione uma opção: "))
+print("")
 
-bot.bot_run()
+if entrada == 1:
+    vagas = scraping.get_vagas()
+    scraping.vagas_to_dict(vagas)
+elif entrada == 2:
+    bot.bot_run()
